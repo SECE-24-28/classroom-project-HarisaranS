2900e1ed66c0e3761906f1d1e88b4e0e15d84b50
Harisaran S <harisaran777s@gmail.com>
2025-12-17 09:09:52 +0530

    Updated MERN

--- MERNStack_Day9/entry.js
+++ MERNStack_Day9/entry.js
@@ -1,25 +1,26 @@
-const express=require('express')
-const app=express()
-const PORT=8001 
+const express = require("express");
+const path = require("path");
 
-app.get('/',(req,res)=>{
-    res.send('Backend server is running successfully')
-})
+const app = express();
+const PORT = 8001;
 
-app.get('/json',(req,res)=>{
-    res.json({
-        "hi":"sece",
-        "branch":"cys",
-        "stuCount":"64"
+app.get("/", (req, res) => {
+  res.send("Backend server is running successfully");
+});
 
+app.get("/json", (req, res) => {
+  res.json({
+    hi: "sece",
+    branch: "cys",
+    stuCount: 64
+  });
+});
 
-})
-    })
+app.get("/html", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
-app.get('/html',(req, res)=>{
-    res.sendFile("C:/Users/thila/Desktop/node/MERN_Backend/index.html")
-})    
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
 
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
-})
